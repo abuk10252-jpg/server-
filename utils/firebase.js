c0717313ef5a50e90e4cc804997756238967a9e3
abuk10252-jpg <abuk10252@gmail.com>
2026-04-27 00:42:53 +0200

    Update firebase.js

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,11 +1,15 @@
 const admin = require("firebase-admin");
-const path = require("path");
 
-const serviceAccount = require(path.join(__dirname, "../firebase-key.json"));
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: `${serviceAccount.project_id}.appspot.com`
+  storageBucket: process.env.STORAGE_BUCKET,
 });
 
 const db = admin.firestore();
